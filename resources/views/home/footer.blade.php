4461978f119016bd9567db2f100019572cf3fcfa
wafaemoueddene <w.moueddene0141@uca.ac.ma>
2025-05-09 13:29:47 +0100

    Modif Footer UI

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -1,59 +1,55 @@
- <!--  footer -->
- <footer>
-         <div class="footer">
-            <div class="container">
-               <div class="row">
-                  <div class=" col-md-4">
-                     <h3>Contact US</h3>
-                     <ul class="conta">
-                        <li><i class="fa fa-map-marker" aria-hidden="true"></i> Address</li>
-                        <li><i class="fa fa-mobile" aria-hidden="true"></i> +01 1234569540</li>
-                        <li> <i class="fa fa-envelope" aria-hidden="true"></i><a href="#"> demo@gmail.com</a></li>
-                     </ul>
-                  </div>
-                  <div class="col-md-4">
-                     <h3>Menu Link</h3>
-                     <ul class="link_menu">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="about.html"> about</a></li>
-                        <li><a href="event.html">Our event</a></li>
-                        <li><a href="gallery.html">Gallery</a></li>
-                        <li><a href="blog.html">Blog</a></li>
-                        <li><a href="contact.html">Contact Us</a></li>
-                     </ul>
-                  </div>
-                  <div class="col-md-4">
-                     <h3>News letter</h3>
-                     <form class="bottom_form">
-                        <input class="enter" placeholder="Enter your email" type="text" name="Enter your email">
-                        <button class="sub_btn">subscribe</button>
-                     </form>
-                     <ul class="social_icon">
-                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
-                     </ul>
-                  </div>
-               </div>
+<!--  footer -->
+<footer>
+   <div class="footer" style="background-color: #000 ; color: #fff;">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-3">
+               <h3 style="color: #f3863f; font-size: 18px; margin-bottom: 20px;">CONTACT</h3>
+               <ul class="conta" style="list-style: none; padding-left: 0;">
+                  <li style="margin-bottom: 8px;"><i class="fa fa-map-marker" aria-hidden="true"></i> 123 Main Street, City</li>
+                  <li style="margin-bottom: 8px;"><i class="fa fa-mobile" aria-hidden="true"></i> +01 234 567 8910</li>
+                  <li style="margin-bottom: 8px;"><i class="fa fa-envelope" aria-hidden="true"></i> <a href="#" style="color: #fff; text-decoration: none;">info@yourwebsite.com</a></li>
+                  <li style="margin-bottom: 8px;"><a href="#" style="color: #fff; text-decoration: none;">www.yourwebsite.com</a></li>
+                  <li style="margin-bottom: 8px;">OPEN: 09:00 AM - 18:00 PM</li>
+               </ul>
             </div>
-            <div class="copyright">
-               <div class="container">
-                  <div class="row">
-                     <div class="col-md-10 offset-md-1">
-                        
-                        
-
-                     </div>
-                  </div>
-               </div>
+            <div class="col-md-3">
+               <h3 style="color: #f3863f; font-size: 18px; margin-bottom :20px;">USEFUL LINKS</h3>
+               <ul class="link_menu" style="list-style: none; padding-left :0;">
+                  <li style="margin-bottom :8px;"><a href="#" style="color: #fff; text-decoration: none;">Home</a></li>
+                  <li style="margin-bottom :8px;"><a href="#about" style="color: #fff; text-decoration: none;">About</a></li>
+                  <li style="margin-bottom :8px;"><a href="#event" style="color: #fff; text-decoration: none;">Our Event</a></li>
+                  <li style="margin-bottom :8px;"><a href="#blog" style="color: #fff; text-decoration: none;">Blog</a></li>
+                  <li style="margin-bottom :8px;"><a href="#contact" style="color: #fff; text-decoration: none;">Contact Us</a></li>
+               </ul>
+            </div>
+            <div class="col-md-3">
+               <img src="images/logo_blackBG.png" alt="Logo" style="width :100%; margin-bottom :20px;">
+            </div>
+            <div class="col-md-3">
+               <h3 style="color: #f3863f; font-size: 18px; margin-bottom :20px;">NEWSLETTER</h3>
+               <form class="newsletter_form">
+                  <input class="enter" placeholder="Enter your email" type="text" name="email" style="width: 100%; padding: 8px; margin-bottom: 10px ; border-radius:10px ;">
+                  <button class="btn" style="background-color: #f3863f; color: #fff; border: none; padding: 8px 15px; cursor: pointer; border-radius:10px; float :right;">SUBSCRIBE</button>
+               </form>
+               <ul class="social_icon" style="list-style: none; padding-right : 0; margin-top: 20px; display: flex;">
+                  <li style="margin-right: 10px;"><a href="#" style="display: inline-block; width: 32px; height: 32px; background-color: #f3863f; color: #fff; text-align: center; line-height: 32px; border-radius: 20px;"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                  <li style="margin-right: 10px;"><a href="#" style="display: inline-block; width: 32px; height: 32px; background-color: #f3863f; color: #fff; text-align: center; line-height: 32px; border-radius: 20px;"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                  <li style="margin-right: 10px;"><a href="#" style="display: inline-block; width: 32px; height: 32px; background-color: #f3863f; color: #fff; text-align: center; line-height: 32px; border-radius: 20px;"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                  <li style="margin-right: 10px;"><a href="#" style="display: inline-block; width: 32px; height: 32px; background-color: #f3863f; color: #fff; text-align: center; line-height: 32px; border-radius: 20px;"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+               </ul>
             </div>
          </div>
-      </footer>
-      <!-- end footer -->
-      <script src="js/jquery.min.js"></script>
-      <script src="js/bootstrap.bundle.min.js"></script>
-      <script src="js/jquery-3.0.0.min.js"></script>
-      <!-- sidebar -->
-      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
-      <script src="js/custom.js"></script>
+      </div>
+
+   </div>
+</footer>
+<!-- end footer -->
+
+<!-- Scripts -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery-3.0.0.min.js"></script>
+<!-- sidebar -->
+<script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="js/custom.js"></script>
